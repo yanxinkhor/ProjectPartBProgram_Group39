62125d191383fc60465519c442c7f499ec27bb50
Khor Yan Xin <yanxin20030528@gmail.com>
2024-11-30 13:07:48 +0800

    The newest Change

--- src/main/java/com/example/projectpartbprogram_group39/DAO/TraineeDaoInterface.java
+++ src/main/java/com/example/projectpartbprogram_group39/DAO/TraineeDaoInterface.java
@@ -5,7 +5,7 @@ import com.example.projectpartbprogram_group39.Models.Trainee;
 import java.io.IOException;
 import java.util.List;
 
-public interface daoInterface {
+public interface TraineeDaoInterface {
     void StoreTrainee(Trainee trainee);
     boolean userExists(String username, String email)throws IOException;
     List<Trainee> getAllTrainees()throws IOException;
